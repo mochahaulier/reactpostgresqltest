37fd08c9aed476c1f681b656119872c5784b4ab0
mochahaulier <mochahaulier@gmail.com>
2024-05-02 17:16:12 +0200

    Removed routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,11 @@ import "./App.css";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <Home />
     </div>
   );
 }
